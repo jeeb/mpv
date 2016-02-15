c48e742df9b7a2828e0614318589ae63d0541f4e
Ilya Zhuravlev <whatever@xyz.is>
2016-02-15 21:24:04 +0300

    ao/opensles: cleanups

--- audio/out/ao_opensles.c
+++ audio/out/ao_opensles.c
@@ -8,9 +8,10 @@
 #include <SLES/OpenSLES_Android.h>
 
 struct priv {
-    SLObjectItf sl;
-    SLPlayItf playItf;
-    SLBufferQueueItf bufferQueueItf;
+    SLObjectItf sl, output_mix, player;
+    SLBufferQueueItf buffer_queue;
+    SLEngineItf engine;
+    SLPlayItf play;
     char *buffer;
     size_t buffer_size;
 };
@@ -22,63 +23,65 @@ static const int fmtmap[][2] = {
     { 0 }
 };
 
+#define DESTROY(thing) \
+    if (p->thing) { \
+        (*p->thing)->Destroy(p->thing); \
+        p->thing = NULL; \
+    }
+
 static void uninit(struct ao *ao)
 {
-    struct priv *priv = ao->priv;
+    struct priv *p = ao->priv;
 
-    if (priv->sl)
-        (*priv->sl)->Destroy(priv->sl);
+    DESTROY(player);
+    DESTROY(output_mix);
+    DESTROY(sl);
 
-    free(priv->buffer);
-    priv->buffer = NULL;
-    priv->buffer_size = 0;
+    p->buffer_queue = NULL;
+    p->engine = NULL;
+    p->play = NULL;
+
+    free(p->buffer);
+    p->buffer = NULL;
+    p->buffer_size = 0;
 }
 
-static void BufferQueueCallback(SLBufferQueueItf queueItf, void *context)
+#undef DESTROY
+
+static void buffer_callback(SLBufferQueueItf buffer_queue, void *context)
 {
     int read;
     struct ao *ao = context;
     struct priv *priv = (struct priv*)ao->priv;
     SLresult res;
     void *data[1] = { priv->buffer };
+
     double delay = priv->buffer_size / (double)ao->bps;
     read = ao_read_data(ao, data, priv->buffer_size / ao->sstride, mp_time_us() + delay);
-    res = (*queueItf)->Enqueue(queueItf, priv->buffer, ao->sstride * read);
+    res = (*buffer_queue)->Enqueue(buffer_queue, priv->buffer, ao->sstride * read);
     if (res != SL_RESULT_SUCCESS)
         MP_ERR(ao, "Failed to Enqueue: %d\n", res);
 }
 
-static void resume(struct ao *ao)
-{
-    SLresult res;
-    struct priv *priv = (struct priv*)ao->priv;
-    SLPlayItf playItf = priv->playItf;
-    res = (*playItf)->SetPlayState(playItf, SL_PLAYSTATE_PLAYING);
-    if (res != SL_RESULT_SUCCESS)
-        MP_ERR(ao, "Failed to SetPlayState(SL_PLAYSTATE_PLAYING): %d\n", res);
-
-    BufferQueueCallback(priv->bufferQueueItf, ao);
-}
-
-#define MAX_INTERFACES 5
 #define BUFFER_SIZE_MS 50
+
+#define CHK(stmt) \
+    { \
+        SLresult res = stmt; \
+        if (res != SL_RESULT_SUCCESS) { \
+            MP_ERR(ao, "%s: %d\n", #stmt, res); \
+            goto error; \
+        } \
+    }
 
 static int init(struct ao *ao)
 {
-    struct priv *priv = ao->priv;
-    SLObjectItf sl, outputmix, player;
-    SLPlayItf playItf;
-    SLEngineItf engine;
-    SLboolean required[MAX_INTERFACES];
-    SLInterfaceID iidArray[MAX_INTERFACES];
-    SLDataLocator_BufferQueue bufferQueue;
+    struct priv *p = ao->priv;
+    SLDataLocator_BufferQueue locator_buffer_queue;
+    SLDataLocator_OutputMix locator_output_mix;
     SLDataFormat_PCM pcm;
-    SLDataSource audioSource;
-    SLDataLocator_OutputMix locator_outputmix;
-    SLDataSink audioSink;
-    SLBufferQueueItf bufferQueueItf;
-    SLresult res;
-    int i;
+    SLDataSource audio_source;
+    SLDataSink audio_sink;
 
     ao->format = af_fmt_from_planar(ao->format);
     switch (ao->format) {
@@ -90,48 +93,17 @@ static int init(struct ao *ao)
         break;
     }
 
-    res = slCreateEngine(&sl, 0, NULL, 0, NULL, NULL);
-    if (res != SL_RESULT_SUCCESS) {
-        MP_ERR(ao, "slCreateEngine failed: %d\n", res);
-        goto error;
-    }
+    CHK(slCreateEngine(&p->sl, 0, NULL, 0, NULL, NULL));
+    CHK((*p->sl)->Realize(p->sl, SL_BOOLEAN_FALSE));
+    CHK((*p->sl)->GetInterface(p->sl, SL_IID_ENGINE, (void*)&p->engine));
+    CHK((*p->engine)->CreateOutputMix(p->engine, &p->output_mix, 0, NULL, NULL));
+    CHK((*p->output_mix)->Realize(p->output_mix, SL_BOOLEAN_FALSE));
 
-    priv->sl = sl;
-
-    res = (*sl)->Realize(sl, SL_BOOLEAN_FALSE);
-    if (res != SL_RESULT_SUCCESS) {
-        MP_ERR(ao, "Failed to Realize the engine: %d\n", res);
-        goto error;
-    }
-
-    res = (*sl)->GetInterface(sl, SL_IID_ENGINE, (void*)&engine);
-    if (res != SL_RESULT_SUCCESS) {
-        MP_ERR(ao, "Failed to get SL_IID_ENGINE interface: %d\n", res);
-        goto error;
-    }
-
-    for (i = 0; i < MAX_INTERFACES; ++i) {
-        required[i] = SL_BOOLEAN_FALSE;
-        iidArray[i] = SL_IID_NULL;
-    }
-
-    res = (*engine)->CreateOutputMix(engine, &outputmix, 0, iidArray, required);
-    if (res != SL_RESULT_SUCCESS) {
-        MP_ERR(ao, "Failed to CreateOutputMix: %d\n", res);
-        goto error;
-    }
-
-    res = (*outputmix)->Realize(outputmix, SL_BOOLEAN_FALSE);
-    if (res != SL_RESULT_SUCCESS) {
-        MP_ERR(ao, "Failed to Realize the Output Mix object: %d\n", res);
-        goto error;
-    }
-
-    bufferQueue.locatorType = SL_DATALOCATOR_BUFFERQUEUE;
-    bufferQueue.numBuffers = 1;
+    locator_buffer_queue.locatorType = SL_DATALOCATOR_BUFFERQUEUE;
+    locator_buffer_queue.numBuffers = 1;
 
     pcm.formatType = SL_DATAFORMAT_PCM;
-    pcm.numChannels = ao->channels.num;
+    pcm.numChannels = 2;
     pcm.samplesPerSec = ao->samplerate * 1000;
     for (int n = 0; fmtmap[n][0]; ++n) {
         if (ao->format == fmtmap[n][0]) {
@@ -144,50 +116,25 @@ static int init(struct ao *ao)
     pcm.endianness = SL_BYTEORDER_LITTLEENDIAN;
 
     ao->device_buffer = ao->samplerate * BUFFER_SIZE_MS / 1000;
-    priv->buffer_size = ao->device_buffer * ao->channels.num * af_fmt_to_bytes(ao->format);
-    priv->buffer = calloc(1, priv->buffer_size);
+    p->buffer_size = ao->device_buffer * ao->channels.num * af_fmt_to_bytes(ao->format);
+    p->buffer = calloc(1, p->buffer_size);
 
-    audioSource.pFormat = (void*)&pcm;
-    audioSource.pLocator = (void*)&bufferQueue;
+    audio_source.pFormat = (void*)&pcm;
+    audio_source.pLocator = (void*)&locator_buffer_queue;
 
-    locator_outputmix.locatorType = SL_DATALOCATOR_OUTPUTMIX;
-    locator_outputmix.outputMix = outputmix;
-    audioSink.pLocator = (void*)&locator_outputmix;
-    audioSink.pFormat = NULL;
+    locator_output_mix.locatorType = SL_DATALOCATOR_OUTPUTMIX;
+    locator_output_mix.outputMix = p->output_mix;
 
-    required[0] = SL_BOOLEAN_TRUE;
-    iidArray[0] = SL_IID_BUFFERQUEUE;
-    res = (*engine)->CreateAudioPlayer(engine, &player, &audioSource, &audioSink, 1, iidArray, required);
-    if (res != SL_RESULT_SUCCESS) {
-        MP_ERR(ao, "Failed to CreateAudioPlayer: %d\n", res);
-        goto error;
-    }
+    audio_sink.pLocator = (void*)&locator_output_mix;
+    audio_sink.pFormat = NULL;
 
-    res = (*player)->Realize(player, SL_BOOLEAN_FALSE);
-    if (res != SL_RESULT_SUCCESS) {
-        MP_ERR(ao, "Failed to Realize the player: %d\n", res);
-        goto error;
-    }
-
-    res = (*player)->GetInterface(player, SL_IID_PLAY, (void*)&playItf);
-    if (res != SL_RESULT_SUCCESS) {
-        MP_ERR(ao, "Failed to GetInterface(player, SL_IID_PLAY): %d\n", res);
-        goto error;
-    }
-    priv->playItf = playItf;
-
-    res = (*player)->GetInterface(player, SL_IID_BUFFERQUEUE, (void*)&bufferQueueItf);
-    if (res != SL_RESULT_SUCCESS) {
-        MP_ERR(ao, "Failed to GetInterface(player, SL_IID_BUFFERQUEUE: %d\n", res);
-        goto error;
-    }
-    priv->bufferQueueItf = bufferQueueItf;
-
-    res = (*bufferQueueItf)->RegisterCallback(bufferQueueItf, BufferQueueCallback, ao);
-    if (res != SL_RESULT_SUCCESS) {
-        MP_ERR(ao, "Failed to register buffer queue callback: %d\n", res);
-        goto error;
-    }
+    SLboolean required[] = { SL_BOOLEAN_TRUE };
+    SLInterfaceID iid_array[] = { SL_IID_BUFFERQUEUE };
+    CHK((*p->engine)->CreateAudioPlayer(p->engine, &p->player, &audio_source, &audio_sink, 1, iid_array, required));
+    CHK((*p->player)->Realize(p->player, SL_BOOLEAN_FALSE));
+    CHK((*p->player)->GetInterface(p->player, SL_IID_PLAY, (void*)&p->play));
+    CHK((*p->player)->GetInterface(p->player, SL_IID_BUFFERQUEUE, (void*)&p->buffer_queue));
+    CHK((*p->buffer_queue)->RegisterCallback(p->buffer_queue, buffer_callback, ao));
 
     return 1;
 error:
@@ -195,14 +142,27 @@ error:
     return -1;
 }
 
+#undef CHK
+
+static void set_play_state(struct ao *ao, SLuint32 state)
+{
+    struct priv *p = ao->priv;
+    SLresult res = (*p->play)->SetPlayState(p->play, state);
+    if (res != SL_RESULT_SUCCESS)
+        MP_ERR(ao, "Failed to SetPlayState(%d): %d\n", state, res);
+}
+
 static void reset(struct ao *ao)
 {
-    SLresult res;
-    struct priv *priv = (struct priv*)ao->priv;
-    SLPlayItf playItf = priv->playItf;
-    res = (*playItf)->SetPlayState(playItf, SL_PLAYSTATE_PAUSED);
-    if (res != SL_RESULT_SUCCESS)
-        MP_ERR(ao, "Failed to SetPlayState(SL_PLAYSTATE_PAUSED): %d\n", res);
+    set_play_state(ao, SL_PLAYSTATE_STOPPED);
+}
+
+static void resume(struct ao *ao)
+{
+    struct priv *p = ao->priv;
+    set_play_state(ao, SL_PLAYSTATE_PLAYING);
+
+    buffer_callback(p->buffer_queue, ao);
 }
 
 const struct ao_driver audio_out_opensles = {
