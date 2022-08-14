df92aa7ad7b8c874f64f72f451410acc98354aaa
wnoun <wnoun@outlook.com>
2022-08-14 10:37:01 +0800

    hwdec_d3d11egl: check for EGL_EXT_device_query in client extensions
    For the longest time, ANGLE exposed this feature as a normal
    display extension while it was defined as a client extension.
    In 56663dbfa780035abf1fff95192c5b2776ec32b1 [¹] in November 2020,
    this was finally fixed, breaking this module's feature check.
    
    Thus, check for the extension in its defined location.
    
    Fixes #10212
    
    [1] https://chromium.googlesource.com/angle/angle/+/56663dbfa780035abf1fff95192c5b2776ec32b1

--- video/out/opengl/hwdec_d3d11egl.c
+++ video/out/opengl/hwdec_d3d11egl.c
@@ -100,12 +100,18 @@ static int init(struct ra_hwdec *hw)
 
     GL *gl = ra_gl_get(hw->ra);
 
+    const char *client_exts = eglQueryString(EGL_NO_DISPLAY, EGL_EXTENSIONS);
     const char *exts = eglQueryString(egl_display, EGL_EXTENSIONS);
     if (!gl_check_extension(exts, "EGL_ANGLE_d3d_share_handle_client_buffer") ||
         !gl_check_extension(exts, "EGL_ANGLE_stream_producer_d3d_texture") ||
         !(gl_check_extension(gl->extensions, "GL_OES_EGL_image_external_essl3") ||
           gl->es == 200) ||
-        !gl_check_extension(exts, "EGL_EXT_device_query") ||
+        // Before 56663dbfa780035abf1fff95192c5b2776ec32b1 (Nov, 2020), ANGLE
+        // implemented EGL_EXT_device_query as a display extension, while its
+        // definition was as a client extension. Thus check for the correct
+        // location first, and then the previous location in case of failure.
+        !(gl_check_extension(client_exts, "EGL_EXT_device_query") ||
+          gl_check_extension(exts, "EGL_EXT_device_query")) ||
         !(gl->mpgl_caps & MPGL_CAP_TEX_RG))
         return -1;
 
